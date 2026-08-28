aa5312fc1f545f244c145c2616447793c029d89c
Nicolas Lanker <Stigz@users.noreply.github.com>
2026-08-28 17:30:12 +0200

    Allow project bucket state inspection

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_role_policy" "github" {
       {
         Sid    = "ManageProjectBucketsAndState"
         Effect = "Allow"
-        Action = ["s3:CreateBucket", "s3:DeleteBucket", "s3:GetBucket*", "s3:GetAccelerateConfiguration", "s3:ListBucket", "s3:PutBucket*", "s3:DeleteBucket*", "s3:GetObject", "s3:GetObjectVersion", "s3:PutObject", "s3:DeleteObject", "s3:ListBucketVersions"]
+        Action = ["s3:CreateBucket", "s3:DeleteBucket", "s3:Get*", "s3:ListBucket", "s3:PutBucket*", "s3:DeleteBucket*", "s3:PutObject", "s3:DeleteObject", "s3:ListBucketVersions"]
         Resource = [
           "arn:aws:s3:::${var.project_name}-*",
           "arn:aws:s3:::${var.project_name}-*/*"
